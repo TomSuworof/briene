086e718bc24dc21f729c934a8edaaf1ac73a86b9
TomSuworof <rc1995king@gmail.com>
2021-08-15 15:17:10 +0300

    vue support for author's page

--- src/main/java/com/salat/briene/api/PageController.java
+++ src/main/java/com/salat/briene/api/PageController.java
@@ -1,12 +1,17 @@
 package com.salat.briene.api;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/experimental")
 public class PageController {
+
     @GetMapping("")
     public String getIndexPage() {
         return "index_vue";
@@ -15,5 +20,11 @@ public class PageController {
     @GetMapping("/articles")
     public String getArticlesPage() {
         return "articles_vue";
+    }
+
+    @GetMapping("/authors/{authorName}")
+    public String getAuthorPage(@PathVariable String authorName, Model model) {
+        model.addAttribute(authorName);
+        return "author_vue";
     }
 }
