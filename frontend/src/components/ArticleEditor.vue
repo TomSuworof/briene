86c1de6676c92e30d00c168de268653e0a11bfb4
Tom Suworof <rc1995king@gmail.com>
2021-08-24 18:40:35 +0300

    in progress #14. Editor is working now

--- frontend/src/components/ArticleEditor.vue
+++ frontend/src/components/ArticleEditor.vue
@@ -2,33 +2,70 @@
   <div class="editor-page-content">
     <form method="post">
       <div class="editor-wrapper">
-        <div>
-          <label for="title"></label>
-          <input id="title" class="title" type="text" placeholder="Title" name="title" required/>
+        <div class="row">
+          <div class="title-wrapper">
+            <input id="title" class="title" type="text" placeholder="Title" name="title" required/>
+          </div>
+          <div class="action-buttons">
+            <input type="submit" name="action" value="Publish"/>
+            <input type="submit" name="action" value="Save"/>
+          </div>
         </div>
-        <div>
-          <label for="editor"></label>
-          <textarea id="editor" placeholder="Content here ..." name="content"></textarea>
+        <div class="editor">
+          <vue-simplemde v-model="content" ref="markdownEditor"/>
         </div>
-      </div>
-      <div class="action-buttons">
-        <input type="submit" name="action" value="Publish"/>
-        <input type="submit" name="action" value="Save"/>
       </div>
     </form>
   </div>
 </template>
 
 <script>
-//import Editor from '/static/js/lepture_editor';
+import VueSimplemde from 'vue-simplemde'
 
 export default {
   name: "ArticleEditor",
+  components: {
+    VueSimplemde
+  },
+  data() {
+    return {
+      content: '',
+    }
+  },
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+  },
+  methods: {},
   created() {
+
+  },
+  beforeMount() {
+    if (this.currentUser === null) {
+      this.$router.push({path: '/login'});
+    }
   }
 }
 </script>
 
 <style scoped>
+
+@import '~simplemde/dist/simplemde.min.css';
+
+.row {
+  padding: 0 10pt 0;
+  justify-content: space-between;
+}
+
+input[type=text] {
+  font-size: 20pt;
+  outline: none;
+  border: none;
+}
+
+.editor-page-content {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+}
 
 </style>
