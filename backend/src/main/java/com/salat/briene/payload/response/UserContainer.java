4096228a27c65e67deee7555074fdf3fc2f75ab1
Tom Suworof <rc1995king@gmail.com>
2021-08-31 10:14:49 +0300

    closes #8. Field email was added in admin dashboard

--- backend/src/main/java/com/salat/briene/payload/response/UserContainer.java
+++ backend/src/main/java/com/salat/briene/payload/response/UserContainer.java
@@ -10,11 +10,13 @@ import java.util.Set;
 public class UserContainer {
     private final Long id;
     private final String username;
+    private final String email;
     private final Set<Role> roles;
 
     public UserContainer(User user) {
         this.id = user.getId();
         this.username = user.getUsername();
+        this.email = user.getEmail();
         this.roles = user.getRoles();
     }
 }
