a1bfd9fa8dae485ad5dc36c0fc74abfb61650c8e
Tom Suworof <rc1995king@gmail.com>
2021-09-11 10:52:17 +0300

    closes #8. Error serialization by class ErrorResponse

--- backend/src/main/java/com/salat/briene/controlleradvices/ArticleAdvice.java
+++ backend/src/main/java/com/salat/briene/controlleradvices/ArticleAdvice.java
@@ -3,6 +3,7 @@ package com.salat.briene.controlleradvices;
 import com.salat.briene.exceptions.ArticleNotFoundException;
 import com.salat.briene.exceptions.DuplicatedArticleException;
 import com.salat.briene.exceptions.IllegalArticleStateException;
+import com.salat.briene.payload.response.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,28 +11,43 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
 @ControllerAdvice
 public class ArticleAdvice {
     private static final Logger logger = LoggerFactory.getLogger(ArticleAdvice.class);
 
     @ExceptionHandler(ArticleNotFoundException.class)
-    public ResponseEntity<?> handleArticleNotFound(ArticleNotFoundException e) {
+    public ResponseEntity<?> handleArticleNotFound(ArticleNotFoundException e, HttpServletRequest request) {
         logger.error("Article not found error: {}", e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse response = new ErrorResponse(new Date(), status.value(), e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(response);
     }
 
     @ExceptionHandler(DuplicatedArticleException.class)
-    public ResponseEntity<?> handleDuplicatedArticle(DuplicatedArticleException e) {
+    public ResponseEntity<?> handleDuplicatedArticle(DuplicatedArticleException e, HttpServletRequest request) {
         logger.error("Duplicated article error: {}", e.getMessage());
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(e.getMessage());
+
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ErrorResponse response = new ErrorResponse(new Date(), status.value(), e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(response);
         // 422 Unprocessable Entity response status code indicates that the server understands the content type of the
         // request entity, and the syntax of the request entity is correct, but it was unable to process the contained
         // instructions, because content is already exists.
     }
 
     @ExceptionHandler(IllegalArticleStateException.class)
-    public ResponseEntity<?> handleIllegalArticleState(IllegalArticleStateException e) {
+    public ResponseEntity<?> handleIllegalArticleState(IllegalArticleStateException e, HttpServletRequest request) {
         logger.error("Illegal article state error: {}", e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErrorResponse response = new ErrorResponse(new Date(), status.value(), e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(response);
     }
 }
